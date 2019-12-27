6035fe6c53c0c5af06c8c81d130d98bef76306ed
albertBaigl <albert_baigl@163.com>
2019-12-27 15:58:54 +0800

    退货

---
+++ pages/salesreturn/salesreturndetail.vue
@@ -0,0 +1,377 @@
+<template>
+	<view>
+		<view class="stata-section">
+			请等待商家审核
+		</view>
+		<view class="goods-section">
+			<!-- 商品列表 -->
+			<view class="g-item">
+				<image src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1620020012,789258862&fm=26&gp=0.jpg"></image>
+				<view class="right">
+					<text class="title clamp">韩版于是洞洞拖鞋 夏季浴室防滑简约居家【新人专享，限选意见】</text>
+					<text class="spec">春装款 L</text>
+					<view class="price-box">
+						<text class="price">￥17.8</text>
+						<text class="number">x 1</text>
+					</view>
+				</view>
+			</view>
+		</view>
+
+		<!-- 优惠明细 -->
+		<view class="yt-list">
+			<view class="yt-list-cell b-b" @click="toggleMask('show')">
+				<text class="cell-tit clamp">退款原因</text>
+				<text class="cell-tip active">
+					请选择退款原因
+				</text>
+				<text class="cell-more wanjia wanjia-gengduo-d"></text>
+			</view>
+			<view class="yt-list-cell b-b">
+				<text class="cell-tit clamp">退款数量</text>
+				<text class="cell-tip disabled">1件</text>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				maskState: 0, //优惠券面板显示状态
+				desc: '', //备注
+				payType: 1, //1微信 2支付宝
+				couponList: [
+					{
+						title: '新用户专享优惠券',
+						price: 5,
+					},
+					{
+						title: '庆五一发一波优惠券',
+						price: 10,
+					},
+					{
+						title: '优惠券优惠券优惠券优惠券',
+						price: 15,
+					}
+				],
+				addressData: {
+					name: '许小星',
+					mobile: '13853989563',
+					addressName: '金九大道',
+					address: '山东省济南市历城区',
+					area: '149号',
+					default: false,
+				}
+			}
+		},
+		onLoad(option){
+			//商品数据
+			//let data = JSON.parse(option.data);
+			//console.log(data);
+		},
+		methods: {
+			//显示优惠券面板
+			toggleMask(type){
+				let timer = type === 'show' ? 10 : 300;
+				let	state = type === 'show' ? 1 : 0;
+				this.maskState = 2;
+				setTimeout(()=>{
+					this.maskState = state;
+				}, timer)
+			},
+			numberChange(data) {
+				this.number = data.number;
+			},
+			changePayType(type){
+				this.payType = type;
+			},
+			submit(){
+				uni.redirectTo({
+					url: '/pages/money/pay'
+				})
+			},
+			stopPrevent(){}
+		}
+	}
+</script>
+
+<style lang="scss">
+	page {
+		background: $page-color-base;
+		padding-bottom: 100upx;
+	}
+
+	.stata-section{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		hight: 500upx;
+		background: red;
+	}
+
+	.address-section {
+		padding: 30upx 0;
+		background: #fff;
+		position: relative;
+
+		.order-content {
+			display: flex;
+			align-items: center;
+		}
+
+		.icon-shouhuodizhi {
+			flex-shrink: 0;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			width: 90upx;
+			color: #888;
+			font-size: 44upx;
+		}
+
+		.cen {
+			display: flex;
+			flex-direction: column;
+			flex: 1;
+			font-size: 28upx;
+			color: $font-color-dark;
+		}
+
+		.name {
+			font-size: 34upx;
+			margin-right: 24upx;
+		}
+
+		.address {
+			margin-top: 16upx;
+			margin-right: 20upx;
+			color: $font-color-light;
+		}
+
+		.icon-you {
+			font-size: 32upx;
+			color: $font-color-light;
+			margin-right: 30upx;
+		}
+
+		.a-bg {
+			position: absolute;
+			left: 0;
+			bottom: 0;
+			display: block;
+			width: 100%;
+			height: 5upx;
+		}
+	}
+
+	.goods-section {
+		margin-top: 16upx;
+		background: #fff;
+		padding-bottom: 1px;
+
+		.g-header {
+			display: flex;
+			align-items: center;
+			height: 84upx;
+			padding: 0 30upx;
+			position: relative;
+		}
+
+		.logo {
+			display: block;
+			width: 50upx;
+			height: 50upx;
+			border-radius: 100px;
+		}
+
+		.name {
+			font-size: 30upx;
+			color: $font-color-base;
+			margin-left: 24upx;
+		}
+
+		.g-item {
+			display: flex;
+			margin: 20upx 30upx;
+			padding: 30upx;
+			image {
+				flex-shrink: 0;
+				display: block;
+				width: 140upx;
+				height: 140upx;
+				border-radius: 4upx;
+			}
+
+			.right {
+				flex: 1;
+				padding-left: 24upx;
+				overflow: hidden;
+			}
+
+			.title {
+				font-size: 30upx;
+				color: $font-color-dark;
+			}
+
+			.spec {
+				font-size: 26upx;
+				color: $font-color-light;
+			}
+
+			.price-box {
+				display: flex;
+				align-items: center;
+				font-size: 32upx;
+				color: $font-color-dark;
+				padding-top: 10upx;
+
+				.price {
+					margin-bottom: 4upx;
+				}
+				.number{
+					font-size: 26upx;
+					color: $font-color-base;
+					margin-left: 20upx;
+				}
+			}
+
+			.step-box {
+				position: relative;
+			}
+		}
+	}
+	.yt-list {
+		margin-top: 16upx;
+		background: #fff;
+	}
+
+	.yt-list-cell {
+		display: flex;
+		align-items: center;
+		padding: 10upx 30upx 10upx 40upx;
+		line-height: 70upx;
+		position: relative;
+
+		&.cell-hover {
+			background: #fafafa;
+		}
+
+		&.b-b:after {
+			left: 30upx;
+		}
+
+		.cell-icon {
+			height: 32upx;
+			width: 32upx;
+			font-size: 22upx;
+			color: #fff;
+			text-align: center;
+			line-height: 32upx;
+			background: #f85e52;
+			border-radius: 4upx;
+			margin-right: 12upx;
+
+			&.hb {
+				background: #ffaa0e;
+			}
+
+			&.lpk {
+				background: #3ab54a;
+			}
+
+		}
+
+		.cell-more {
+			align-self: center;
+			font-size: 24upx;
+			color: $font-color-light;
+			margin-left: 8upx;
+			margin-right: -10upx;
+		}
+
+		.cell-tit {
+			flex: 1;
+			font-size: 26upx;
+			color: $font-color-light;
+			margin-right: 10upx;
+		}
+
+		.cell-tip {
+			font-size: 26upx;
+			color: $font-color-dark;
+
+			&.disabled {
+				color: $font-color-light;
+			}
+
+			&.active {
+				color: $base-color;
+			}
+			&.red{
+				color: $base-color;
+			}
+		}
+
+		&.desc-cell {
+			.cell-tit {
+				max-width: 90upx;
+			}
+		}
+
+		.desc {
+			flex: 1;
+			font-size: $font-base;
+			color: $font-color-dark;
+		}
+	}
+	
+	/* 支付列表 */
+	.pay-list{
+		padding-left: 40upx;
+		margin-top: 16upx;
+		background: #fff;
+		.pay-item{
+			display: flex;
+			align-items: center;
+			padding-right: 20upx;
+			line-height: 1;
+			height: 110upx;	
+			position: relative;
+		}
+		.icon-weixinzhifu{
+			width: 80upx;
+			font-size: 40upx;
+			color: #6BCC03;
+		}
+		.icon-alipay{
+			width: 80upx;
+			font-size: 40upx;
+			color: #06B4FD;
+		}
+		.icon-xuanzhong2{
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			width: 60upx;
+			height: 60upx;
+			font-size: 40upx;
+			color: $base-color;
+		}
+		.tit{
+			font-size: 32upx;
+			color: $font-color-dark;
+			flex: 1;
+		}
+	}
+	
+	.border{
+		width: 100%;
+		height: 2upx;
+		background: #c8c7cc;
+	}
+
+</style>
