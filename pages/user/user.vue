ea464eace040f82093e05831244072aac59b3361
2622595880 <2622595880@qq.com>
2020-01-03 09:54:44 +0800

    我的

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -84,8 +84,8 @@
 					<image @click="navTo('/pages/product/product')" src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2691146630,2165926318&fm=26&gp=0.jpg" mode="aspectFill"></image>
 					<image @click="navTo('/pages/product/product')" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1553105443324&di=8141bf13f3f208c61524d67f9bb83942&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01ac9a5548d29b0000019ae98e6d98.jpg" mode="aspectFill"></image>
 					<image @click="navTo('/pages/product/product')" src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=191678693,2701202375&fm=26&gp=0.jpg" mode="aspectFill"></image>
-				</scroll-view> -->
-				<list-cell icon="icon-iconfontweixin" iconColor="#e07472" title="我的订单" @eventClick="navTo('/pages/order/order?state=0')"></list-cell>
+				</scroll-view> @eventClick="navTo('/pages/order/order?state=0')"-->
+				<list-cell icon="icon-iconfontweixin" iconColor="#e07472" title="我的订单" @eventClick="MyOrder()"></list-cell>
 				<list-cell icon="icon-dizhi" iconColor="#5fcda2" title="配送地址" @eventClick="navTo('/pages/address/address')"></list-cell>
 				<list-cell icon="icon-share" iconColor="#9789f7" title="通知" ></list-cell>
 				<list-cell icon="icon-pinglun-copy" iconColor="#ee883b" title="隐私" ></list-cell>
@@ -164,6 +164,17 @@
 			   }
 			  
 		    },
+			//我的订单
+			MyOrder(){
+				console.log(this.hasLogin)
+				if(this.hasLogin){
+					uni.navigateTo({
+						url: '/pages/order/order?state=0'
+					})
+				}else {
+						uni.showModal({title: "请登录!"});
+					}
+			},
 			doregister(){
 				if(this.register=="注册"){
 				 uni.navigateTo({
